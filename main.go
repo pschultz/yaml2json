66b9c0f7db8d37950beafe7c83922df2124427c8
Peter Schultz <peter.schultz@classmarkets.com>
2016-08-29 18:24:48 +0200

    add -c flag and filename arg

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,28 +12,65 @@ import (
 	"github.com/ghodss/yaml"
 )
 
+func main() {
+	prog := filepath.Base(os.Args[0])
+
+	log.SetFlags(0)
+	log.SetPrefix(prog + ": ")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: %s <OPTIONS> <FILENAME>
+
+Convert the yaml contents of FILENAME to json and print it on stdout. If
+FILENAME is '-' or omitted, stdin is read instead.
+
+OPTIONS:
+`, prog)
+		flag.PrintDefaults()
+	}
+
+	compact := flag.Bool("c", false, "Print compact instead of pretty json.")
+
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 1 {
+		log.Fatalf("expected at most one argument; the name of a yaml file")
+	}
+
+	b, err := readInput(args)
+	check(err)
+
+	b, err = yaml.YAMLToJSON(b)
+	check(err)
+
+	if !*compact {
+		b, err = makePretty(b)
+		check(err)
+	}
+
+	os.Stdout.Write(b)
+	os.Stdout.Write([]byte{'\n'})
+}
+
+func readInput(args []string) ([]byte, error) {
+	if len(args) > 0 && args[0] != "-" {
+		return ioutil.ReadFile(args[0])
+	} else {
+		return ioutil.ReadAll(os.Stdin)
+	}
+}
+
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func main() {
-	log.SetFlags(0)
-	log.SetPrefix(filepath.Base(os.Args[0]) + ": ")
-
-	b, err := ioutil.ReadAll(os.Stdin)
-	check(err)
-
-	b, err = yaml.YAMLToJSON(b)
-	check(err)
-
+func makePretty(b []byte) ([]byte, error) {
 	var doc *json.RawMessage
-	err = json.Unmarshal(b, &doc)
-	check(err)
+	if err := json.Unmarshal(b, &doc); err != nil {
+		return b, err
+	}
 
-	b, err = json.MarshalIndent(doc, "", "  ")
-	check(err)
-
-	fmt.Fprintf(os.Stdout, "%s\n", b)
+	return json.MarshalIndent(doc, "", "  ")
 }
